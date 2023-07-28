0261a687c893ce69499a4d1bed5c082240382b6f
SriramKumar <gode.srk@gmail.com>
2023-07-28 07:26:54 +0530

    firstcommit

---
+++ src/DataStructures/Arrays/Client.java
@@ -0,0 +1,9 @@
+package DataStructures.Arrays;
+
+import java.sql.SQLOutput;
+
+public class Client {
+    public static void main(String[] args) {
+        System.out.println("This package belongs to Arrays DS");
+    }
+}
